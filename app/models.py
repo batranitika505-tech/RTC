895515c70d57868832b4130b0fe40c037bce1082
varunvaruns9 <varunsinghs2021@gmail.com>
2019-04-17 19:56:09 +0530

    Add models.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class CrackLog(models.Model):
+	date = models.CharField(max_length=20)
+	time = models.CharField(max_length=20)
+	location = models.CharField(max_length=100)
+
+
+class ObstacleLog(models.Model):
+	date = models.CharField(max_length=20)
+	time = models.CharField(max_length=20)
+	location = models.CharField(max_length=100)
