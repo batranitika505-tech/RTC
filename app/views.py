ee4f42b602c329af4bbe64b91aebe8f3949b8777
varunvaruns9 <varunsinghs2021@gmail.com>
2019-04-20 11:27:49 +0530

    Import status module.

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Crack, Obstacle
 from .serializers import CrackSerializer, ObstacleSerializer
 
