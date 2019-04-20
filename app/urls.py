01b43d0812a4aca5ab52f2d1b739ff0cc75b8e1d
varunvaruns9 <varunsinghs2021@gmail.com>
2019-04-20 12:29:44 +0530

    Change login view.

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
 
 
 urlpatterns = [
-    path('obstacle', views.ObstacleView.as_view()),
-    path('crack', views.CrackView.as_view()),
+    path('obstacle/', views.ObstacleView.as_view()),
+    path('crack/', views.CrackView.as_view()),
     path('', views.index, name='index'),
-    path('', include('django.contrib.auth.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
