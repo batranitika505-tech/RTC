bf8d782ec2b17be36fba11aa1e8049ccd17e8ed2
varunvaruns9 <varunsinghs2021@gmail.com>
2019-04-20 11:18:40 +0530

    Add API views.

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('obstacle', views.ObstacleView.as_view()),
     path('crack', views.CrackView.as_view()),
--- app/views.py
+++ app/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Crack, Obstacle
+from .serializers import CrackSerializer, ObstacleSerializer
 
-# Create your views here.
+
+class CrackView(APIView):
+
+	def get(self, request, format=None):
+		cracks = Crack.objects.all()
+		serializer = CrackSerializer(cracks, many=True)
+		return Response(serializer.data)
+
+	def post(self, request, format=None):
+		serializer = CrackSerializer(data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ObstacleView(APIView):
+
+	def get(self, request, format=None):
+		obstacles = Obstacle.objects.all()
+		serializer = ObstacleSerializer(obstacles, many=True)
+		return Response(serializer.data)
+
+	def post(self, request, format=None):
+		serializer = ObstacleSerializer(data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
