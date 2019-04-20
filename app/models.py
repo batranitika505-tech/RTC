57122c796aa410d39a490a0781d327d353d2c089
varunvaruns9 <varunsinghs2021@gmail.com>
2019-04-20 11:22:47 +0530

    Make model migrations.

--- app/models.py
+++ app/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 
-class CrackLog(models.Model):
+class Crack(models.Model):
 	date = models.CharField(max_length=20)
 	time = models.CharField(max_length=20)
 	location = models.CharField(max_length=100)
 
 
-class ObstacleLog(models.Model):
+class Obstacle(models.Model):
 	date = models.CharField(max_length=20)
 	time = models.CharField(max_length=20)
 	location = models.CharField(max_length=100)
