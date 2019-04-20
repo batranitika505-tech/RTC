6209545f6d9915edeb001175309396856693f1e8
varunvaruns9 <varunsinghs2021@gmail.com>
2019-04-20 11:41:40 +0530

    Add index view.

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('obstacle', views.ObstacleView.as_view()),
     path('crack', views.CrackView.as_view()),
+    path('', views.index, name='index'),
+    path('login', views.login, name='login'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render, redirect
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -33,3 +34,12 @@ class ObstacleView(APIView):
 			serializer.save()
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def index(request):
+	if request.user.is_authenticated:
+		cracks = Crack.objects.all()
+		obstacles = Obstacle.objects.all()
+		render(request, 'index.html', {'cracks': cracks, 'obstacles': obstacles})
+	else:
+		redirect('login')
